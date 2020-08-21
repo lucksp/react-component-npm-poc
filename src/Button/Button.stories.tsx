eaf3b5f6c05ceedd899e893c7fe7d5db991e7254
Phil Lucks <plucks@gogoair.com>
2020-08-21 09:06:15 -0600

    stylelint

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,20 +1,7 @@
 import React, { FC } from 'react';
 
 import { Button } from './Button';
-// import { Story } from '@storybook/react/types-6-0';
-// import { ButtonProps } from './Button.types';
 import { action } from '@storybook/addon-actions';
-
-// // This default export determines where you story goes in the story list
-// const Template: Story<ButtonProps> = args => <Button {...args} />;
-
-// // Each story then reuses that template
-// export const Primary = Template.bind({});
-// Primary.args = {
-//   testId: 'storybook-test',
-//   text: 'Click',
-//   onClick: action('clicked'),
-// };
 
 export default {
   title: 'Button',
--- src/Button/StyledButton.css.ts
+++ src/Button/StyledButton.css.ts
@@ -4,4 +4,5 @@ export const StyledButton = styled.button`
   font-size: 14px;
   background-color: '#764abc';
   color: '#eee';
+  border: none;
 `;
