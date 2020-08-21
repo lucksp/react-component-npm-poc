acc2f6426056d7f65ea2f433dc355a104060fc93
Phil Lucks <plucks@gogoair.com>
2020-08-21 09:42:32 -0600

    button style

--- src/Button/StyledButton.css.ts
+++ src/Button/StyledButton.css.ts
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
-  font-size: 14px;
-  background-color: '#764abc';
-  color: '#eee';
+  background-color: #764abc;
   border: none;
+  border-radius: 4px;
+  color: #eee;
+  cursor: pointer;
+  font-size: 14px;
+  padding: 8px 16px;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #5f2caf;
+    transition: background-color 0.2s;
+  }
 `;
