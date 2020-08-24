e934d652791dae98d5749e92cf044dd0f390be2d
Phil Lucks <plucks@gogoair.com>
2020-08-24 08:47:18 -0600

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
-const packageJson = await import('./package.json');
+// this override is needed because Module format cjs does not support top-level await
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json');
 
 export default {
   input: 'src/index.ts',
@@ -19,11 +21,15 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript()],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+  ],
 };
 
 // Other useful plugins you might want to add are:
-
 // @rollup/plugin-images - import image files into your components
 // @rollup/plugin-json - import JSON files into your components
 // rollup-plugin-terser - minify the Rollup bundle
